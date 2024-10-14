5b7fcde0f0b82e98e5cb1c200d4adb3cf1eb9d65
Moazam05 <salmanmoazam08@gmail.com>
2024-10-14 17:14:03 +0500

    navigationRef

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './MainNavigator';
+import {prepareNavigation} from '../utils/NavigationUtils';
 
 const Navigation: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={prepareNavigation}>
       <MainNavigation />
     </NavigationContainer>
   );
