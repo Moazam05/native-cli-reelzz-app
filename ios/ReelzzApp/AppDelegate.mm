30cf282a215a33fbc9f4fc107d31290da35f61ca
Moazam05 <salmanmoazam08@gmail.com>
2024-10-14 14:57:33 +0500

    permission added

--- ios/ReelzzApp/AppDelegate.mm
+++ ios/ReelzzApp/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <AVFoundation/AVFoundation.h> 
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -7,6 +7,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"ReelzzApp";
+   // You can add your custom initial props in the dictionary below.
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
