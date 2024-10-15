79e1fc7952fa1bce75ee1bde3cff7b91bb91e409
Moazam05 <salmanmoazam08@gmail.com>
2024-10-15 15:19:35 +0500

    breathing animation

--- src/screens/auth/SplashScreen.tsx
+++ src/screens/auth/SplashScreen.tsx
@@ -1,12 +1,64 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Animated, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Colors} from '../../constants/Colors';
+import Logo from '../../assets/images/logo_t.png';
 
 const SplashScreen = () => {
+  const [isStop, setIsStop] = useState(false);
+  const scale = new Animated.Value(1);
+
+  useEffect(() => {
+    const breathingAnimation = Animated.loop(
+      Animated.sequence([
+        Animated.timing(scale, {
+          toValue: 1.1, // Scale up
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scale, {
+          toValue: 1, // Scale down
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ]),
+    );
+
+    if (!isStop) {
+      breathingAnimation.start();
+    }
+
+    return () => {
+      breathingAnimation.stop();
+    };
+  }, [isStop]);
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Animated.Image
+          source={Logo}
+          style={{
+            width: '60%',
+            height: '25%',
+            resizeMode: 'contain',
+            transform: [{scale}],
+          }}
+        />
+      </View>
     </View>
   );
 };
 
 export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
